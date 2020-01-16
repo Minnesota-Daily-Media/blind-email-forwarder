72cb8fa1d0a3975a5928c21a13ac5f0216ebaea6
Nick Knudsen <nickknudsen3@gmail.com>
2020-01-16 14:06:17 -0600

    Added functionality to list unread messages in Gmail inbox

--- src/main.py
+++ src/main.py
@@ -8,33 +8,43 @@ from google.auth.transport.requests import Request
 SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
 
 def main():
-	creds = None
-	if os.path.exists('token.pickle'):
-		with open('token.pickle', 'rb') as token:
-			creds = pickle.load(token)
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
 
-	if not creds or not creds.valid:
-		if creds and creds.expired and creds.refresh_token:
-			creds.refresh(Request())
-		else:
-			flow = InstalledAppFlow.from_client_secrets_file(
-				'credentials.json', SCOPES)
-			creds = flow.run_local_server(port=0)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
 
-		with open('token.pickle', 'wb') as token:
-			pickle.dump(creds, token)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
 
-	service = build('gmail', 'v1', credentials=creds)
+    service = build('gmail', 'v1', credentials=creds)
 
-	results = service.users().labels().list(userId='me').execute()
-	labels = results.get('labels', [])
+    label_ids = ['UNREAD']
 
-	if not labels:
-		print('No labels found.')
-	else:
-		print('Labels:')
-		for label in labels:
-			print(label['name'])
+    results = service.users().messages().list(userId='me', labelIds=label_ids).execute()
+    messages = []
+
+    if 'messages' in results:
+        messages.extend(results['messages'])
+    while 'nextPageToken' in results:
+        page_token = results['nextPageToken']
+        results = service.users().messages().list(userId='me', labelIds=label_ids).execute()
+
+        messages.extend(results[messages])
+
+    if not messages:
+        print('No unread messages found.')
+    else:
+        print('Message Ids:')
+        for messageId in messages:
+            print(messageId['id'])
 
 if __name__ == '__main__':
-	main()
+    main()
